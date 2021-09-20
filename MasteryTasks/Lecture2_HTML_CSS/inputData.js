4da58add29c70afd5c2265172fa9bc076e9d13b6
MalachiParks1999 <MalachiParks1999@gmail.com>
2021-09-19 22:15:47 -0400

    Website now fully works with user input on click

--- MasteryTasks/Lecture2_HTML_CSS/inputData.js
+++ MasteryTasks/Lecture2_HTML_CSS/inputData.js
@@ -1,6 +1,29 @@
+/*
+Tasks: Write a website that asks someone their name, favorite color, and birthday. 
+Change the background of the web page to match their favorite color and display their birthday and name. 
+Try this with several types of events (blur, change, or clicking a button)
+*/
+
 function changeBGColor(){
     let color = document.getElementById("favColor").value;
     document.body.style.backgroundColor = color;
 }
 
-document.querySelector("#submitButton").addEventListener("click", changeBGColor);
+function displayName(){
+    let name = document.getElementById("name").value;
+    document.querySelector("#userName").innerHTML = "Hello " + name;
+}
+
+function displayBday(){
+    let birthday = document.getElementById("bday").value;
+    document.querySelector("#userBday").innerHTML = "DOB:" + birthday;
+}
+
+function clickHandler(){
+    changeBGColor();
+    displayBday();
+    displayName();
+}
+
+// click listner to change color
+document.querySelector("#submitButton").addEventListener("click", clickHandler);
