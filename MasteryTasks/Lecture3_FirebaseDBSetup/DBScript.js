5586a82d39475690a4afb0e18d514f88eeed7762
MalachiParks1999 <MalachiParks1999@gmail.com>
2021-09-21 22:52:02 -0400

    Changing DBScript to .js script from config for it to work

--- MasteryTasks/Lecture3_FirebaseDBSetup/DBScript.js
+++ MasteryTasks/Lecture3_FirebaseDBSetup/DBScript.js
@@ -1,45 +1,21 @@
-<script type="module">
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-  // Import the functions you need from the SDKs you need
-
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
-
-  // TODO: Add SDKs for Firebase products that you want to use
-
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-  // Your web app's Firebase configuration
-
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-  const firebaseConfig = {
+const firebaseConfig = {
 
     apiKey: "AIzaSyBUPSkWopoWxow-JF1COYbDDkniq7XitOo",
-
+  
     authDomain: "mparks-fbrtdbex.firebaseapp.com",
-
+  
     databaseURL: "https://mparks-fbrtdbex-default-rtdb.firebaseio.com",
-
+  
     projectId: "mparks-fbrtdbex",
-
+  
     storageBucket: "mparks-fbrtdbex.appspot.com",
-
+  
     messagingSenderId: "490911240079",
-
+  
     appId: "1:490911240079:web:1c7eb6d5bfe6579c9c45fa",
-
+  
     measurementId: "G-KBS26KCSXH"
-
+  
   };
-
-
-  // Initialize Firebase
-
-  const app = initializeApp(firebaseConfig);
-
-  const analytics = getAnalytics(app);
-
-</script>
